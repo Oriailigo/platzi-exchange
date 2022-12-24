469e3c61bedf2aa2db6a50516718113365ee3fb6
ori <ori_larusa@hotmail.com>
2022-12-24 14:59:19 -0300

    se crearon las rutas de about, home y error

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
+//rutas
+
+import router from "@/components/router";
 Vue.config.productionTip = false;
 
 new Vue({
+  router: router,
   render: (h) => h(App),
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <px-assets-table> </px-assets-table>
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+import api from "@/api";
+export default {
+  name: "Home",
+  components: { PxAssetsTable },
+  //guardar dato
+  data() {
+    return {
+      assets: [],
+    };
+  },
+  //hook para hacer la interaccion con la api y poder setear mi array.
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets));
+  },
+};
+</script>
