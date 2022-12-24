0553b2318b220cec8a30c684f30209c49152ae7e
ori <ori_larusa@hotmail.com>
2022-12-24 15:50:22 -0300

    se agregaron las rutas de about, home y error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,19 +6,9 @@
 
 <script>
 import PxAssetsTable from "@/components/PxAssetsTable";
-import api from "@/api";
+
 export default {
   name: "Home",
   components: { PxAssetsTable },
-  //guardar dato
-  data() {
-    return {
-      assets: [],
-    };
-  },
-  //hook para hacer la interaccion con la api y poder setear mi array.
-  created() {
-    api.getAssets().then((assets) => (this.assets = assets));
-  },
 };
 </script>
