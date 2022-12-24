cb952dea9a91c2da1e0e04eab8553f8a7c91f97b
ori <ori_larusa@hotmail.com>
2022-12-24 16:58:42 -0300

    se realizo el fetch a la api pero no muestra la informacion guardada

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,18 @@
 
 <script>
 import PxAssetsTable from "@/components/PxAssetsTable";
-
+import api from "@/api";
 export default {
   name: "Home",
   components: { PxAssetsTable },
+  data() {
+    return {
+      assets: [],
+    };
+  },
+
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets));
+  },
 };
 </script>
